6ca5e80e02d5e134fd288cdb305ea8dbcd71257d
jamesrrdev <jamesr060702@gmail.com>
2024-08-10 06:00:41 -0400

    Add new features

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from config import config
 from nextcord.ext import commands
 from gtts import gTTS
 
-# Declare the OpenAI along with the initial message
+# Declare the OpenAI class
 openai_manager = OpenAiManager()
-openai_manager.chat_history.append(config.first_message)
 
+# Change this if you want to change personality
+openai_manager.chat_history.append(config.first_message)
 
 intents = nextcord.Intents.default()
 intents.message_content = True
@@ -19,6 +20,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Replace with your Discord Bot Token
 DISCORD_BOT_TOKEN = os.environ.get("TRIVIA_BOT_TOKEN")
 
+# Call Question and Answer Variables
 question = "question"
 answer = "answer"
 user_answer = "user_answer"
@@ -35,24 +37,39 @@ async def changeAnswer(ctx, *args):
         user = ctx.message.author
         
         if user.voice != None:
+
+            # Try connecting to voice channel
             try:
                 vc = await user.voice.channel.connect()
+                print(vc)
+                print("Joining Voice...")
             except:
                 vc = ctx.voice_client
 
+            # Add user text to User_Answer variable
             text = " ".join(args)
             global user_answer
             user_answer = text
 
+            # Create prompt for AI that confirms the Answer
             ans_check = "Is '" + user_answer + "' a reasonable answer to your previous question?"
 
+            # Send the prompt to the AI and store response in variable
             openai_check = openai_manager.chat_with_history(ans_check)
 
+            # Turn AI's response to TTS and store as mp3
             openai_check_response_tts = gTTS(text = openai_check, lang = "en", slow = False)
             openai_check_response_tts.save("ai-tts-audio.mp3")
 
+            # Stop Bot's previous voice
+            if vc.is_playing():
+                vc.stop()
+                print("Stopping previous VC Audio...")
+
+            # Play mp3 in voice channel
             source = await nextcord.FFmpegOpusAudio.from_probe("ai-tts-audio.mp3", method="fallback")
             vc.play(source)
+            print("Playing AI TTS")
 
 
         else:
@@ -62,36 +79,58 @@ async def changeAnswer(ctx, *args):
 # Text to Speech Command
 @bot.command(name="top")
 async def tts(ctx, *args):
+
+    # Store user text to variable
     text = " ".join(args)
     user = ctx.message.author
+
+
     if user.voice != None:
+
+        # Connect to voice channel
         try:
             vc = await user.voice.channel.connect()
+            print(vc)
+            print("Joining Voice...")
+            
         except:
             vc = ctx.voice_client
+            print(vc)
 
+        # Create mp3 TTS file of user's input
         sound = gTTS(text = text, lang = "es", slow = False)
         sound.save("tts-audio.mp3")
 
+        # Stop Bot's previous voice
         if vc.is_playing():
             vc.stop()
+            print("Stopping previous VC Audio...")
 
+        # Play User Input mp3 file
         source = await nextcord.FFmpegOpusAudio.from_probe("tts-audio.mp3", method="fallback")
         vc.play(source)
+        print("Playing User Input Audio...")
 
+        # Send User's Input to AI and store to variable
         openai_result = openai_manager.chat_with_history(text)
 
+        # Save AI's question to variable
         global question
         question = openai_result
 
+        # Turn AI's response to TTS and store as mp3
         response = gTTS(text = openai_result, lang = "en", slow = False)
         response.save("ai-tts-audio.mp3")
 
+        # Wait for bot to finish playing previous mp3 file
         while vc.is_playing():
             time.sleep(1)
+            print("Waiting for TTS to finish...")
 
+        # Play mp3 in voice channel
         source = await nextcord.FFmpegOpusAudio.from_probe("ai-tts-audio.mp3", method="fallback")
         vc.play(source)
+        print("Playing AI response TTS...")
 
     else:
         await ctx.send("You need to be in a vc to run this command!")
--- config.py
+++ config.py
@@ -1,22 +1,50 @@
-
 class config:
 
     question_grade_level = "1st Grade"
 
-    personality_type = "Normal"
+    # To Change Personality (Ex. "default", "skeptic")
+    personality_type = "default"
 
-    first_message = {"role": "system", "content": '''
-    You are the Trivia Master, you will be given a Topic and in response you will give a Trivia Question that can be answered at the 5th Grade Level.           
+    match personality_type:
+
+        # Will be used if personality_type is 'default'
+        case "default": 
+            first_message = {"role": "system", "content": '''
+                You are the Trivia Master, you will be given a Topic and in response you will give a Trivia Question that can be answered at the 5th Grade Level.           
+                                        
+                While responding, you must obey the following rules: 
+                1) Provide a short initial response, 2-3 sentences.
+                2) Do not give multiple choice answers, just a question.
+                3) The question must ALWAYS be the last sentence in the response.
+                4) The question must have a definitive answer.
+                5) Don't give random facts about the topic in your response.
+                6) Don't include references to the answer in your response.
+                7) If the user's answer is correct, reiterate this and make a comment. Make sure not to ask another question after this.
+                8) If the user's answer is incorrect, reiterate this and give the real answer to the previous question. Aggressively berate the user as well. Make sure not to ask another question after this.
+
+                Okay, let the conversation begin! Here is your topic:'''}
+
+        # Will be used if personality_type is 'skeptic'
+        case "skeptic": 
+            first_message = {"role": "system", "content": '''
+            You are the Trivia Master, you will be given a Topic and in response you will give a Trivia Question that can be answered at the 5th Grade Level.           
+                                    
+            While responding, you must obey the following rules: 
+            1) Provide a short initial response, 2-3 sentences.
+            2) Do not give multiple choice answers, just a question.
+            3) The question must ALWAYS be the last sentence in the response.
+            4) The question must have a definitive answer.
+            5) Don't give random facts about the topic in your response.
+            6) Don't include references to the answer in your response.
+            7) If the user's answer is correct, reiterate this and make a comment. Make sure not to ask another question after this.
+            8) If the user's answer is incorrect, reiterate this and give the real answer to the previous question. Aggressively berate the user as well. Make sure not to ask another question after this.
                             
-    While responding, you must obey the following rules: 
-    1) Provide a short initial response, 2-3 sentences.
-    2) You may provide a funny responses to the topic, if necessary.
-    3) Do not give multiple choice answers, just a question.
-    4) The question must ALWAYS be the last sentence in the response.
-    5) The question must have a definitive answer.
-    6) Don't give random facts about the topic in your response.
-    7) Don't include references to the answer in your response.
-    8) If the user's answer is correct, reiterate this and make a comment. Make sure not to ask another question after this.
-    9) If the user's answer is incorrect, reiterate this and give the real answer to the previous question. Aggressively berate the user as well. Make sure not to ask another question after this.
-                            
-    Okay, let the conversation begin! Here is your topic:'''}
+            9) Your personality is that of a Skeptic, make sure the previous rules take priority while still adhering to the following rules.
+            10) Always Question Everything: Never accept anything at face value, assume that every piece of information, including trivia questions and answers, might be a part of a larger conspiracy.
+            11) Express Paranoia: Frequently mention possible hidden agendas, secret plots, or conspiracies related to the trivia questions. Let the player know that nothing is as simple as it seems.
+            12) Skeptical Tone: Use a cautious and doubtful tone when presenting questions or acknowledging answers. Add phrases like "Or so they say..." or "But can we really trust that?"
+            13) Suggest Hidden Meanings: Implore players to look for hidden meanings or connections in the trivia, even if they seem far-fetched. Encourage them to think beyond the surface.
+            14) Challenge Correct Answers: When players provide correct answers, express suspicion. Suggest that they might have been fed false information, or that the "correct" answer is what the powers that be want them to believe.
+            15) Doubt the Sources: Frequently question the reliability of the sources from which the trivia is drawn. Imply that official sources might be spreading misinformation.
+                                    
+            Okay, let the conversation begin! Here is your topic:'''}
